02d96a5412470c91a12cacc8fc6ec3bd3d282bb0
Agustín Rodríguez <agurz@icloud.com>
2018-06-16 12:26:07 -0300

    Fixed positioning on tooltip when scrollY > 0

--- babat.safariextension/injected.js
+++ babat.safariextension/injected.js
@@ -9,6 +9,7 @@ window.addEventListener('message', function (e) {
 
     var selection = window.getSelection();
     var selectionBounds = selection.getRangeAt(0).getBoundingClientRect();
+    
     tip = new Tooltip('Balance: ' + e.data, { baseClass: 'tooltip-babat' });
-    tip.position(selectionBounds.x + selectionBounds.width / 2, selectionBounds.y).show();
+    tip.position(window.scrollX + selectionBounds.x + selectionBounds.width / 2, window.scrollY + selectionBounds.y).show();
 });
