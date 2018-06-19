d1aa97af67471fcde5c4a3753c66d76178e827e1
Agustín Rodríguez <agurz@icloud.com>
2018-06-18 22:21:30 -0300

    Don't show tooltip if there is no selected text

--- babat.safariextension/injected.js
+++ babat.safariextension/injected.js
@@ -8,8 +8,11 @@ window.addEventListener('message', function (e) {
     if (e.origin != window.location.origin) return;
 
     var selection = window.getSelection();
-    var selectionBounds = selection.getRangeAt(0).getBoundingClientRect();
+    var textSelection = selection.toString();
     
-    tip = new Tooltip('Balance: ' + e.data, { baseClass: 'tooltip-babat' });
-    tip.position(window.scrollX + selectionBounds.x + selectionBounds.width / 2, window.scrollY + selectionBounds.y).show();
+    if (textSelection.length) {
+        var selectionBounds = selection.getRangeAt(0).getBoundingClientRect();
+        tip = new Tooltip('Balance: ' + e.data, { baseClass: 'tooltip-babat' });
+        tip.position(window.scrollX + selectionBounds.x + selectionBounds.width / 2, window.scrollY + selectionBounds.y).show();
+    }
 });
